b11d49db9b4b96d78b1531e322ac28d728227fa6
ZyntharSekki <zyntharsekki@gmail.com>
2019-01-22 17:19:57 -0500

    add many stuff

---
+++ kornum.py
@@ -0,0 +1,12 @@
+"testing"
+
+수사_종류 = ["양수사", "양수사-관형사", "서수사", "서수사-명사"]
+
+
+def convert(number, 수사="양수사", 한자어=True):
+    if isinstance(number, str):
+        number = int(number)
+    if 수사 not in 수사_종류:
+        raise NotImplementedError('없는 수사 종류입니다')
+
+    return [number, 수사, 한자어]
