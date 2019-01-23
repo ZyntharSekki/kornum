5a31e667d389c467d4dff4987953caa69ed46792
ZyntharSekki <zyntharsekki@gmail.com>
2019-01-23 13:16:57 -0500

    add test

--- test/test.py
+++ test/test.py
@@ -1,4 +1,5 @@
 일의자리_한자어 = {
+    0 : '',
     1 : '일',
     2 : '이',
     3 : '삼',
@@ -11,9 +12,10 @@
 }
 
 큰_자릿수 = {
-    10 : "십",
-    100 : "백",
-    1000 : "천"
+    0 : "",
+    1 : "십",
+    2 : "백",
+    3 : "천"
 }
 
 a = str(input("입력바람: "))
@@ -24,7 +26,27 @@ for i in a:
     for x in i:
         b.append(x)
 
-for i in range(0, len(b)):
-    result.append(일의자리_한자어.get(int(b.pop())))
+for i in b:
+    result.append(일의자리_한자어.get(int(i)))
 
-print(result)
+theresult = []
+
+for i in range(0, len(result)):
+    if len(result) >= 4:
+        rtemp = result[-4:]
+        for i in range(0, 4):
+            itemp = rtemp.pop()
+            if(itemp == ''):
+                theresult.append(itemp)   
+            elif(itemp == '일'):
+                if i == 0:
+                    theresult.append(itemp)
+                else:
+                    theresult.append(큰_자릿수.get(i))
+            else:
+                theresult.append(itemp + 큰_자릿수.get(i))
+        for i in range(0, 4):
+            result.pop()
+
+
+print(theresult)
